76aa8c2a23fbf2c6527e4121b8613c56788983db
Astha Gupta <42895038+aasthaguptaa@users.noreply.github.com>
2021-11-22 11:42:18 +0530

    Add files via upload

---
+++ JokeGenerator.js
@@ -0,0 +1,22 @@
+
+const jokeText = document.querySelector('.joke-text');
+const newJokeBtn = document.querySelector('.new-joke-btn');
+newJokeBtn.addEventListener('click', getJoke);
+
+getJoke();
+
+function getJoke() {
+  fetch('https://icanhazdadjoke.com/', {
+    headers: {
+      'Accept': 'application/json'
+    }
+  }).then(function(response) {
+    return response.json();
+  }).then(function(data) {
+    const joke = data.joke;
+    jokeText.innerText = joke;
+  }).catch(function(error) {
+    jokeText.innerText = 'Oops! Some error happened :(';
+    console.log(error);
+  });
+}
